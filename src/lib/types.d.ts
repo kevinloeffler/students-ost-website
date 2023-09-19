037685cbf2a49b446f01a9f8f55b5c4c90fcaf79
Kevin Loeffler <hi@kevinloeffler.ch>
2023-09-19 22:26:50 +0200

    add type declarations

---
+++ src/lib/types.d.ts
@@ -0,0 +1,40 @@
+type VereinEvent = {
+    name: string,
+    description: string,
+}
+
+type User = {
+    username: string,
+    password: string,
+    group: AccessGroup,
+}
+
+type Verein = {
+    name: string,
+    description: string,
+}
+
+type AccessToken = {
+    group: AccessGroup,
+    vereinID: null | string,
+}
+
+type AccessGroup =
+    'USER' |    // default value for unauthorized users
+    'ADMIN' |   // for vereins administrators
+    'ROOT'      // for website administrators (students only)
+
+type OstEvent = {
+    name: string,
+    date: Date,
+    description: string,
+    mainImage?: any, // todo: add correct type
+    startTime?: number,
+    endTime?: number,
+    entranceFee?: number,
+    contactEmail?: string,
+    contactPhone?: string,
+    organiser?: string,
+    linkName?: string,
+    link?: string,
+}
