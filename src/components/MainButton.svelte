e2997250de37d1b642fd6abda46c7d04c2543a88
Kevin Loeffler <hi@kevinloeffler.ch>
2023-09-20 14:04:22 +0200

    add button component

---
+++ src/components/MainButton.svelte
@@ -0,0 +1,28 @@
+<a href="{link}">{content}</a>
+
+
+<script lang="ts">
+
+    export let link: string
+    export let content: string
+
+</script>
+
+
+<style>
+
+    a {
+        width: 100%;
+        padding: 12px 20px;
+        margin-bottom: 8px;
+
+        display: flex;
+        justify-content: center;
+        align-content: center;
+
+        font-weight: 700;
+        color: white;
+        background-color: var(--accent-color);
+    }
+
+</style>
