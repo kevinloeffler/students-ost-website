262eeeaee3b225344e9304747efc5607e81582a0
Kevin Loeffler <hi@kevinloeffler.ch>
2023-10-04 14:50:53 +0200

    add the option to pad the date string

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,9 +1,27 @@
-export function formatDateToString(rawDate: string | Date, omitYear: boolean = false, shortYear: boolean = false): string {
+export function formatDateToString(rawDate: string | Date, omitYear: boolean = false, shortYear: boolean = false, addPadding: boolean = false): string {
     const date = new Date(rawDate)
+    let dateStrings: string[]
 
-    if (omitYear) return `${date.getDate()}.${date.getMonth() + 1}`
+    if (omitYear) {
+        dateStrings = [date.getDate().toString(), (date.getMonth() + 1).toString()]
+    } else if (shortYear) {
+        dateStrings = [date.getDate().toString(), (date.getMonth() + 1).toString(), date.getFullYear().toString().slice(2, 4)]
+    } else {
+        dateStrings = [date.getDate().toString(), (date.getMonth() + 1).toString(), date.getFullYear().toString()]
+    }
 
-    if (shortYear) return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear().toString().slice(2, 4)}`
-
-    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
+    return concatDateString(dateStrings, addPadding)
 }
+
+function concatDateString(dateStrings: string[], addPadding: boolean): string {
+    let formattedString = ''
+
+    for (let dateString of dateStrings) {
+        formattedString += addPadding ? dateString.padStart(2, '0') : dateString
+        formattedString += '.'
+    }
+
+    return formattedString.slice(0, formattedString.length - 1)
+}
+
+// TODO: add tests
