6cbce8ff6c441b534f7e0eccb02784ce0b1cc879
Kevin Loeffler <hi@kevinloeffler.ch>
2023-10-04 16:40:31 +0200

    add starthilfe

---
+++ src/routes/starthilfe/+page.svelte
@@ -0,0 +1,49 @@
+<NavBar />
+
+<section class="default-section">
+    <div class="default-container">
+
+        <h1>Starthilfe</h1>
+
+        <p>Zu Beginn deines Studiums ist vieles neu und man erhält viele Informationen auf einen Schlag. Damit du dir nicht alles merken musst, haben wir dir hier die wichtigsten Infos welche du immer wieder brauchst zusammengefasst.</p>
+
+        <h2>Badge</h2>
+        <p>Du kannst ganz einfach über die HSR-App (wenn du mit dem HSR Netzwerk verbunden bist) oder über HSR Intern dein Guthaben auf deinem Badge abfragen.
+            Einsprachen bei falschen Abzügen müssen grundsätzlich innerhalb von drei Arbeitstagen gemeldet werden. (https://badgeportal.hsr.ch)
+
+            Deinen Badge kannst du im neuen Semester bei den Hochschuldiensten validieren lassen. Das Gerät findest du vor dem Schalter der Hochschuldienste.
+
+            Wenn du deinen Badge mit dem VSHSR-Kleber verloren hast, kannst du uns eine E-Mail schreiben und du bekommst einen neuen, kostenlosen Kleber.
+
+            Der Ersatz deines Badge kostet CHF 20.-
+        </p>
+
+        <h2>usw</h2>
+        <p>todo...</p>
+
+    </div>
+</section>
+
+<Footer />
+
+
+<script lang="ts">
+    import Footer from '$lib/components/Footer.svelte'
+    import NavBar from '$lib/components/NavBar.svelte'
+</script>
+
+
+<style>
+
+    h1 {
+        font-family: "Radwave", sans-serif;
+        font-size: 40px;
+    }
+
+    h2 {
+        font-family: "Radwave", sans-serif;
+        font-size: 32px;
+        margin-top: 20px;
+    }
+
+</style>
